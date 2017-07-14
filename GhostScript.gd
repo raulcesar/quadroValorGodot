71a09ce54f01b825bc2e52ed7c41b4ed0f925305
Raul Teixeira <raul.teixeira@camara.leg.br>
2017-07-14 12:05:45 -0300

    incluindo coisas

--- GhostScript.gd
+++ GhostScript.gd
@@ -1,47 +1,47 @@
-extends Sprite
+# extends Sprite
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-var viewportSize
-var moving = false
+# # class member variables go here, for example:
+# # var a = 2
+# # var b = "textvar"
+# var viewportSize
+# var moving = false
 
-func _ready():
-	viewportSize = self.get_parent().get_rect().size
-	var middleOfScene = Vector2(viewportSize.width/2, viewportSize.height/2)
-	self.set_pos(middleOfScene)
-	self.set_process(true)
-	self.set_process_input(true)
-	# self.set_rot(deg2rad(45))
-	# self.set_scale(Vector2(2, 2))
-func move(delta): 
-	var mySize = self.get_item_rect().size
-	var curPos = self.get_pos()
-	curPos.x += 100 * delta
-	if (curPos.x  > viewportSize.width + mySize.width / 2):
-		curPos.x = 0 - mySize.width /2
-	self.set_pos(curPos)
+# func _ready():
+# 	print(get_node("/root"))
+# 	viewportSize = self.get_parent().get_rect().size
+# 	print(viewportSize)
+# 	#var middleOfScene = Vector2(viewportSize.width/2, viewportSize.height/2)
+# 	#self.set_pos(middleOfScene)
+# 	#self.set_process(true)
+# 	#self.set_process_input(true)
+# 	# self.set_rot(deg2rad(45))
+# 	# self.set_scale(Vector2(2, 2))
+# func move(delta): 
+# 	var mySize = self.get_item_rect().size
+# 	var curPos = self.get_pos()
+# 	curPos.x += 100 * delta
+# 	if (curPos.x  > viewportSize.width + mySize.width / 2):
+# 		curPos.x = 0 - mySize.width /2
+# 	self.set_pos(curPos)
 
-func _input(event):
-	if (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.MOUSE_BUTTON):
-		print(str("SCREEN_TOUCH"))
-		self.moving = !self.moving
+# func _input(event):
+# 	if (event.type == InputEvent.SCREEN_TOUCH || event.type == InputEvent.MOUSE_BUTTON):
+# 		print(str("SCREEN_TOUCH"))
+# 		self.moving = !self.moving
 	
 	
-func _process(delta):
-	if (Input.is_key_pressed(KEY_ESCAPE)):
-		self.get_tree().quit()
+# func _process(delta):
 
-	if (Input.is_key_pressed(KEY_LEFT)):
-		self.moving = false
-		print(str("moving: ", self.moving))
+# 	if (Input.is_key_pressed(KEY_LEFT)):
+# 		self.moving = false
+# 		print(str("moving: ", self.moving))
 		
-	if (Input.is_key_pressed(KEY_RIGHT)):
-		self.moving = true
-		print(str("moving: ", self.moving))
+# 	if (Input.is_key_pressed(KEY_RIGHT)):
+# 		self.moving = true
+# 		print(str("moving: ", self.moving))
 		
-	if (self.moving):
-		self.move(delta)
+# 	if (self.moving):
+# 		self.move(delta)
 		
 		
 	
@@ -49,7 +49,7 @@ func _process(delta):
 	
 	
 	
-# func _draw():
-	# self.draw_rect(self.get_item_rect(), Color(0, 0, 1, 0.2))
+# # func _draw():
+# 	# self.draw_rect(self.get_item_rect(), Color(0, 0, 1, 0.2))
 	
 	
---
+++ startup.gd
@@ -0,0 +1,31 @@
+extends Node
+
+# class member variables go here, for example:
+# var a = 2
+var fullscreen = false
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	var root = get_tree().get_root()
+	#OS.set_window_fullscreen(true)
+	set_process_input(true)
+	print("hello all")
+	
+func _input(event):
+	#print(self.fullscreen)
+	
+	
+	if (event.type == InputEvent.KEY ):
+		if (event.scancode == KEY_ESCAPE && !event.is_pressed()):
+			var viewportSize = self.get_parent().get_rect().size
+			print(viewportSize)
+			if (!OS.is_window_fullscreen()):
+				print("Setting fullscreen")
+				OS.set_window_fullscreen(true)
+			else:
+				print("should have reduced")
+				OS.set_window_fullscreen(false)
+	pass
+	
+
